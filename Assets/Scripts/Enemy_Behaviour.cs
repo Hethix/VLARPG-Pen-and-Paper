83798d813f8a0334cd84ec00389f9b9f223aa1d2
ChoosingOwl <lkan12@student.aau.dk>
2017-04-24 15:46:33 +0200

    Start på wandering enemies
    Har lavet lidt NavMesh og lavet et script der skal gøre sådan at enemies
    går rundt og chiller - men det er ikke færdigt endnu.

---
+++ Assets/Scripts/Enemy_Behaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Behaviour : MonoBehaviour {
+
+
+
+   private Vector3 start_pos;
+    private Vector3 rnd_dir;
+    private float roamRadius = 5;
+   
+
+    // Use this for initialization
+    void Start () {
+        start_pos = transform.position;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+       
+       
+
+        Debug.Log(rnd_dir);
+	}
+
+   public void FreeRoam()
+    {
+        rnd_dir = Random.insideUnitSphere * roamRadius;
+        rnd_dir += start_pos;
+        
+        //NavMeshHit hit;
+        //NavMesh.SamplePosition(rnd_dir, out hit, roamRadius, 1);
+        //Vector3 finalPosition = hit.position;
+       //_nav.destination = finalPosition;
+    }
+}
+
+
