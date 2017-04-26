0da1c95ea2ba6fad9deeb5f9106d58f18ccae06e
AndersonDamN <a0@l4l.dk>
2017-04-26 14:16:25 +0200

    Classes, Player HUD, MagusInteraction, Skybox
    The player HUD needs testing in VR environment.
    MagusInteraction / Fireballs needs testing in VR.
    The skybox includes to "alternative" skyboxes, due to the Edge of the
    World problem still persisting.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        StartCoroutine(DestroyAfterTime(5));
+    }
+
+    // Update is called once per frame
+    void Update () {
+        rb.AddForce(gameObject.transform.forward * 8);
+
+
+    }
+
+
+    private IEnumerator DestroyAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+
+}
