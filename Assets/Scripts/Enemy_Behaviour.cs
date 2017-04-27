8f9ae241f98a8bd98744cbf2caace0bcc5bf7d15
ChoosingOwl <lkan12@student.aau.dk>
2017-04-27 12:16:08 +0200

    More Enemies Work
    waddup! - stadigvæk igang

--- Assets/Scripts/Enemy_Behaviour.cs
+++ Assets/Scripts/Enemy_Behaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy_Behaviour : MonoBehaviour {
 
@@ -8,33 +9,46 @@ public class Enemy_Behaviour : MonoBehaviour {
 
    private Vector3 start_pos;
     private Vector3 rnd_dir;
-    private float roamRadius = 5;
-   
+
+    //Påvirker radius hvor den kan roame selve den større cirkel
+    private float roamRadius = 100;
+   NavMeshAgent agent;
+   private Vector3 finalPosition;
+    private float timer;
+    private float wanderTimer = 5.0f;
+
 
     // Use this for initialization
     void Start () {
+       agent = GetComponent<NavMeshAgent>();
         start_pos = transform.position;
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
-       
+
        
 
-        Debug.Log(rnd_dir);
-	}
+        Debug.Log(start_pos);
 
-   public void FreeRoam()
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            agent.SetDestination(finalPosition);
+            timer = 0;
+            newDestination();
+        }
+        NavMeshHit hit;
+        NavMesh.SamplePosition(rnd_dir, out hit, roamRadius, 1);
+        finalPosition = hit.position;
+    }
+
+    public void newDestination()
     {
         rnd_dir = Random.insideUnitSphere * roamRadius;
         rnd_dir += start_pos;
-        
-        //NavMeshHit hit;
-        //NavMesh.SamplePosition(rnd_dir, out hit, roamRadius, 1);
-        //Vector3 finalPosition = hit.position;
-       //_nav.destination = finalPosition;
     }
 }
 
