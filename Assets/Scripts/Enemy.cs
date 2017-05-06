b8456d5c3f985930850e6d87ac772178cff10495
CGLab <asdfsdf@sdfgdfg.gj>
2017-05-06 14:36:48 +0200

    small fix

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -116,7 +116,7 @@ public class Enemy : Character {
         foreach (var obj in players)
         {
             player = obj.GetComponent<Player>();
-            if (GetDistance(gameObject, obj) < 0.1f && player.inStealth==false)
+            if (GetDistance(gameObject, obj) < 25 && player.inStealth==false)
             {
                 combatReady = true;
                 timer = 3.5f; 
