db42522845178ec7cedc17c1430b62fe1bcdc2bc
CGLab <asdfsdf@sdfgdfg.gj>
2017-05-06 12:18:20 +0200

    Character update

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -32,7 +32,7 @@ public class NetworkManager : MonoBehaviour {
             PhotonNetwork.Instantiate("NetworkedGameMaster", Vector3.zero, Quaternion.identity, 0);
         } else if (PhotonNetwork.playerList.Length < 3)
         {
-            PhotonNetwork.Instantiate("NetworkedWarrior", Vector3.zero, Quaternion.identity, 0);
+            PhotonNetwork.Instantiate("NetworkedMagus", Vector3.zero, Quaternion.identity, 0);
         }
         else if (PhotonNetwork.playerList.Length < 4)
         {
