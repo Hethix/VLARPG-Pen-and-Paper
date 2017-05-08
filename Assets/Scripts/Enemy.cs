eae838ae1425001586e8637968ad2ac9f095b7fd
CGLab <asdfsdf@sdfgdfg.gj>
2017-05-08 13:42:25 +0200

    Player fix 0.5

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -47,7 +47,6 @@ public class Enemy : Character {
         giveNewDestination = false;
         setPlayerHP = false;
         performBumpAttack = false;
-        number = GetComponent<NetworkedInteractable>().number; 
     }
 
 	void Update () {
