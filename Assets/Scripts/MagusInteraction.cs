91c4bc939b2e995e4760bfc478bed284ae0af764
ChoosingOwl <lkan12@student.aau.dk>
2017-05-10 12:04:13 +0200

    Healing fix - magus

--- Assets/Scripts/MagusInteraction.cs
+++ Assets/Scripts/MagusInteraction.cs
@@ -10,27 +10,27 @@ public class MagusInteraction : PlayerInteraction
     public override void Update()
     {
         device = SteamVR_Controller.Input((int)trackedObject.index);
-
-        // Should move the player depending on direction of controller + touchpad direction 
+        // Should move the player depending on direction of gaze. Consider making it 
         if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad) && (device.GetAxis().x != 0 || device.GetAxis().y != 0))
         {
             Move(device.GetAxis().x, device.GetAxis().y);
         }
 
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))  //The player has to press before collision. 
+        {
+            if (selfPlayer.CheckCooldown() == true)
+            {
 
-        // Delays the heal until the user is holding the controller inside the other player and pressing the trigger. 
-        // Note: Hasn't been tested yet. 
-            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) //The player has to press before collision. 
-                if (selfPlayer.CheckCooldown() == true)
-                {
-                    selfPlayer.Healing(chara);
-                    selfPlayer.healPlayer = true;
-                    Debug.Log("Healing was applied...");                                                        // Print
-                }
-
+                selfPlayer.Healing(chara);
+                networkPlayer.lastHitPlayer = chara;
+            }
+        }
+        
         // Search by use of the ApplicationMenu
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
             selfPlayer.Search();
+        }
 
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
             ThrowFireball();
