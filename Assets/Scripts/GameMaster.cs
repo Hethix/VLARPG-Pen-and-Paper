9a27479e7cf17cb32e5bba078679147566908782
Andreas Ryge <An.ryge@gmail.com>
2017-05-11 17:47:04 +0200

    Small change to interactables already in scene from start

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour {
 
+    NetworkedInteractable[] allInteractables;
+
 	// Use this for initialization
 	void Start () {
-		
+        allInteractables = FindObjectsOfType<NetworkedInteractable>();
+        foreach(var interactableInArray in allInteractables)
+        {
+            interactableInArray.areGameMaster = true;
+            interactableInArray.avatar = null;
+            interactableInArray.transform.parent = null;
+        }
 	}
 	
 	// Update is called once per frame
